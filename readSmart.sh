e1ca7eb83bfb69b4cd8e0c5dfa9e5d460351ddac
DeSqBlocki <53398124+DeSqBlocki@users.noreply.github.com>
2023-03-26 10:20:25 +0200

    Update readSmart.sh

--- readSmart.sh
+++ readSmart.sh
@@ -10,8 +10,10 @@ FROM= # Mail Sender
 for disk in $disks
 do
         echo "[/dev/$disk]:" >> $file # save locally
+        echo "ID# ATTRIBUTE_NAME          FLAG     VALUE WORST THRESH TYPE      UPDATED  WHEN_FAILED RAW_VALUE" >> $file # SMART value headers
         $sctl -a /dev/$disk | grep "0x00" | grep -v capabilities >> $file # should only return all SMART values
         echo "" >> $file # line break for clarity
+        echo "Num  Test_Description    Status                  Remaining  LifeTime(hours)  LBA_of_first_error" >> $file # SMART test headers
         $sctl -a /dev/$disk | grep "Extended offline" | head -1 >> $file # gets latest 'Extended offline' test result
         echo "" >> $file # line break for clarity
 done
