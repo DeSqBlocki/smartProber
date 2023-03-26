c9db3bf7ca637a84bf5bc13188d6a075324d339c
DeSqBlocki <53398124+DeSqBlocki@users.noreply.github.com>
2023-03-26 10:14:32 +0200

    Update readSmart.sh

--- readSmart.sh
+++ readSmart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-disks='sda sdb sdc sdd sde'
+disks='' #example: 'sda sdb sdc sdd sde'
 sctl=$(which smartctl)
 dir=/root/Logs # Logging Directory
 date=$(date +"%d-%m-%Y")
@@ -10,7 +10,8 @@ FROM= # Mail Sender
 for disk in $disks
 do
         echo "[/dev/$disk]:" >> $file # save locally
-        $sctl -a /dev/$disk | grep "0x00" >> $file # should return all SMART compatible values
+        $sctl -a /dev/$disk | grep "0x00" | grep -v capabilities >> $file # should only return all SMART values
+        echo "" >> $file # line break for clarity
         $sctl -a /dev/$disk | grep "Extended offline" | head -1 >> $file # gets latest 'Extended offline' test result
         echo "" >> $file # line break for clarity
 done
