dae16145f0cc6dd998e4dffbff92f2bece72a97c
DeSqBlocki <53398124+DeSqBlocki@users.noreply.github.com>
2023-03-26 11:15:56 +0200

    Update readSmart.sh

--- readSmart.sh
+++ readSmart.sh
@@ -11,10 +11,11 @@ for disk in $disks
 do
         echo "[/dev/$disk]:" >> $file # save locally
         echo "ID# ATTRIBUTE_NAME          FLAG     VALUE WORST THRESH TYPE      UPDATED  WHEN_FAILED RAW_VALUE" >> $file # SMART value headers
-        $sctl -a /dev/$disk | grep "   0x00" | grep -v capabilities | column -t >> $file # should only return SMART values
+        $sctl -a /dev/$disk | grep "   0x00" | grep -v capabilities>> $file # should only return SMART values
         echo "" >> $file # line break for clarity
         echo "Num  Test_Description    Status                  Remaining  LifeTime(hours)  LBA_of_first_error" >> $file # SMART test headers
-        $sctl -a /dev/$disk | grep "Extended offline" | head -1 | column -t >> $file # gets latest 'Extended offline' test result
+        $sctl -a /dev/$disk | grep "Extended offline" | head -1 >> $file # gets latest 'Extended offline' test result
         echo "" >> $file # line break for clarity
 done
-s-nail -s "SMART Results" -r $FROM $TO < $file
+column -t $file # make it look better
+s-nail -s "SMART Results from $date" -a $file -r $FROM $TO < /dev/null
