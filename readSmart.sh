008d96d04e28da204fd44e99293a4ca5d58f367b
DeSqBlocki <53398124+DeSqBlocki@users.noreply.github.com>
2023-03-26 10:09:40 +0200

    Update readSmart.sh

--- readSmart.sh
+++ readSmart.sh
@@ -10,7 +10,7 @@ FROM= # Mail Sender
 for disk in $disks
 do
         echo "[/dev/$disk]:" >> $file # save locally
-        $sctl -a /dev/$disk | grep "0x00" | head -1 >> $file # should return all SMART compatible values
+        $sctl -a /dev/$disk | grep "0x00" >> $file # should return all SMART compatible values
         $sctl -a /dev/$disk | grep "Extended offline" | head -1 >> $file # gets latest 'Extended offline' test result
         echo "" >> $file # line break for clarity
 done
